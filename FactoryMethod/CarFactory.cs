8adf2e8a6e4f96b743d233f525aad8fdc9b3fab6
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-03 08:39:48 -0500

    Create CarFactory class

---
+++ FactoryMethod/CarFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class CarFactory : TransportFactory
+    {
+        public override ITransport CreateTransport()
+        {
+            return new Car();
+        }
+    }
+}
