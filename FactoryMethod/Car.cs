193d2a009b52aa2f084380099670acf571128767
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-03 08:31:16 -0500

    implement Car class

---
+++ FactoryMethod/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Car : ITransport
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Delivery by car");
+        }
+    }
+}
