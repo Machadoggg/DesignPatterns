aa3ee002653029c333531bad04da0fd24e1bd70b
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 22:19:09 -0500

    concrete class ModernCoffeeTable

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/ModernCoffeeTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class ModernCoffeeTable : ICoffeTable
+    {
+        public void PlaceItemOn()
+        {
+            Console.WriteLine("Placing items on a modern coffe table");
+        }
+    }
+}
