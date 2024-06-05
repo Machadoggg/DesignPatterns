429e61030fada98d046f4ee9313c6273cf5dd187
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 18:48:54 -0500

    Create common interface for user form

---
+++ DesignPatterns.CreationalPattern.Prototype/IUserForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.Prototype
+{
+    public interface IUserForm : ICloneable
+    {
+        string UserName { get; set;  }
+        string Email { get; set; }
+
+        void ShowDetails();
+    }
+}
