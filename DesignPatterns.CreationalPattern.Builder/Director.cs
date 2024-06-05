0fff4d127e9e4d062419ad49b38c350e8dab81e4
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 10:04:34 -0500

    Implement Director class

---
+++ DesignPatterns.CreationalPattern.Builder/Director.cs
@@ -0,0 +1,41 @@
+﻿namespace DesignPatterns.CreationalPattern.Builder
+{
+    public class Director
+    {
+        private IHouseBuilder _builder;
+
+        public Director(IHouseBuilder builder)
+        {
+            _builder = builder;
+        }
+
+
+        public void Construct(bool hasGarage, bool hasSwimPool, bool hasStatues, bool hasGarden)
+        {
+            _builder.BuildWalls()
+                    .BuildDoors()
+                    .BuildWindows()
+                    .BuildRoof();
+
+            if (hasGarage)
+            {
+                _builder.BuildGarage();
+            }
+
+            if (hasSwimPool)
+            {
+                _builder.BuildSwimPool();
+            }
+
+            if (hasStatues)
+            {
+                _builder.BuildStatues();
+            }
+
+            if (!hasGarden)
+            {
+                _builder.BuildGarden();
+            }
+        }
+    }
+}
