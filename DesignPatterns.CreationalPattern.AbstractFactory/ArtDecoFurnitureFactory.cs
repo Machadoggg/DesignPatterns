572c591eddb5273399a1885c00a5b7b7398e9ca4
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 23:37:31 -0500

    Add concret factory Art Deco

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/ArtDecoFurnitureFactory.cs
@@ -0,0 +1,28 @@
+﻿using DesignPatterns.CreationalPattern.AbstractFactory.ConcretClassProducts_ArtDeco;
+using DesignPatterns.CreationalPattern.AbstractFactory.ProductsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class ArtDecoFurnitureFactory : IFurnitureFactory
+    {
+        public IChair CreateChair()
+        {
+            return new ArtDecoChair();
+        }
+
+        public ICoffeTable CreateCoffeTable()
+        {
+            return new ArtDecoCoffeeTable();
+        }
+
+        public ISofa CreateSofa()
+        {
+            return new ArtDecoSofa();
+        }
+    }
+}
