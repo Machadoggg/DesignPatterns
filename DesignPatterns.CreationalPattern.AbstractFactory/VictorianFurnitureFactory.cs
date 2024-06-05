4ee9cc6de705d941cafe90198b3f5d0d1fece9be
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 22:52:34 -0500

    ConcreteFactory_VictorianFurnitureFactory

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/VictorianFurnitureFactory.cs
@@ -0,0 +1,28 @@
+﻿using DesignPatterns.CreationalPattern.AbstractFactory.ConcretClassProducts_Victorian;
+using DesignPatterns.CreationalPattern.AbstractFactory.ProductsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class VictorianFurnitureFactory : IFurnitureFactory
+    {
+        public IChair CreateChair()
+        {
+            return new VictorianChair();
+        }
+
+        public ICoffeTable CreateCoffeTable()
+        {
+            return new VictorianCoffeeTable();
+        }
+
+        public ISofa CreateSofa()
+        {
+            return new VictorianSofa();
+        }
+    }
+}
