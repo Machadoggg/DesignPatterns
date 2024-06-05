b4386eb0d6cd5a69a4dacdc2470b7c87bec310a9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 22:24:03 -0500

    Concrete class VictorianSofa

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/VictorianSofa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class VictorianSofa : ISofa
+    {
+        public void LieOn()
+        {
+            Console.WriteLine("Lying on a victorian sofa.");
+        }
+    }
+}
