013eb6768683e23dc445406991afdbfd4a8c83d8
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 10:31:35 -0500

    Use Builder pattern in main

--- DesignPatterns.CreationalPattern.Builder/Program.cs
+++ DesignPatterns.CreationalPattern.Builder/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatterns.CreationalPattern.Builder;
+
+var builder = new HouseBuilder();
+var director = new Director(builder);
+
+// Build a house with: Garage, SwimPool and Garden
+director.Construct(hasGarage: true, hasSwimPool: true, hasStatues: false, hasGarden: true);
+House house = builder.GetHouse();
+
+house.ShowDetails();
