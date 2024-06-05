f86e1e8f612ba9ee01b50290484f3233a6c3cb9f
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 23:01:20 -0500

    Implement class to handle customer orders

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/FurnitureStore.cs
@@ -0,0 +1,30 @@
+﻿using DesignPatterns.CreationalPattern.AbstractFactory.ProductsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class FurnitureStore
+    {
+        private readonly IFurnitureFactory _furnitureFactory;
+
+        public FurnitureStore(IFurnitureFactory furnitureFactory)
+        {
+            _furnitureFactory = furnitureFactory;
+        }
+
+        public void ProcessOrder()
+        {
+            IChair chair = _furnitureFactory.CreateChair();
+            ISofa sofa = _furnitureFactory.CreateSofa();
+            ICoffeTable coffeTable = _furnitureFactory.CreateCoffeTable();
+
+            chair.SitOn();
+            sofa.LieOn();
+            coffeTable.PlaceItemsOn();
+        }
+    }
+}
