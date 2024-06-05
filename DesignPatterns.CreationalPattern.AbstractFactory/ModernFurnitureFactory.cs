034198bf7d9ec794a207b26a77e8ea87464a205e
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 22:49:46 -0500

    ConcreteFactory_ModernFurnitureFactory

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/ModernFurnitureFactory.cs
@@ -0,0 +1,28 @@
+﻿using DesignPatterns.CreationalPattern.AbstractFactory.ConcretClassProducts_Modern;
+using DesignPatterns.CreationalPattern.AbstractFactory.ProductsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class ModernFurnitureFactory : IFurnitureFactory
+    {
+        public IChair CreateChair()
+        {
+            return new ModernChair();
+        }
+
+        public ICoffeTable CreateCoffeTable()
+        {
+            return new ModernCoffeeTable();
+        }
+
+        public ISofa CreateSofa()
+        {
+            return new ModernSofa();
+        }
+    }
+}
