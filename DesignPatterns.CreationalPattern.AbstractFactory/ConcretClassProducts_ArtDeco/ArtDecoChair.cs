809eac8972a076a7b5b56a6c5e3b64fb7c0a09f1
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 23:33:47 -0500

    Add 3 concret class for Art Deco

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/ConcretClassProducts_ArtDeco/ArtDecoChair.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.CreationalPattern.AbstractFactory.ProductsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory.ConcretClassProducts_ArtDeco
+{
+    public class ArtDecoChair : IChair
+    {
+        public void SitOn()
+        {
+            Console.WriteLine("Sitting on an ART DECO chair.");
+        }
+    }
+}
---
+++ DesignPatterns.CreationalPattern.AbstractFactory/ConcretClassProducts_ArtDeco/ArtDecoCoffeeTable.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.CreationalPattern.AbstractFactory.ProductsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory.ConcretClassProducts_ArtDeco
+{
+    public class ArtDecoCoffeeTable : ICoffeTable
+    {
+        public void PlaceItemsOn()
+        {
+            Console.WriteLine("Placing items on an ART DECO coffee table.");
+        }
+    }
+}
