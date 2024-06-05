a8494fb737cc3646a9185ccd3373acfd945310ed
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 22:22:09 -0500

    Concrete class VictorianChair

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/VictorianChair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class VictorianChair : IChair
+    {
+        public void SitOn()
+        {
+            Console.WriteLine("Sitting on a victorian chair.");
+        }
+    }
+}
