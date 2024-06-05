d38e0b9569b8e01ac3d48c6592fc66ccf014eb1d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 18:58:41 -0500

    User External Form Class

---
+++ DesignPatterns.CreationalPattern.Prototype/BasicUserForm.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPatterns.CreationalPattern.Prototype
+{
+    public class BasicUserForm : IUserForm
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+
+
+        public BasicUserForm() { }
+
+        public BasicUserForm(BasicUserForm source)
+        {
+            if (source != null)
+            { 
+                UserName = source.UserName;
+                Email = source.Email;
+                Password = source.Password;
+            } 
+        }
+
+
+        public object Clone()
+        {
+            return new BasicUserForm(this);
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Basic User Form: Username: {UserName}, Email: {Email}, Password: {Password}");
+        }
+    }
+}
--- DesignPatterns.CreationalPattern.Prototype/IUserForm.cs
+++ DesignPatterns.CreationalPattern.Prototype/IUserForm.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPatterns.CreationalPattern.Prototype
+﻿namespace DesignPatterns.CreationalPattern.Prototype
 {
     public interface IUserForm : ICloneable
     {
-        string UserName { get; set;  }
+        string UserName { get; set; }
         string Email { get; set; }
 
         void ShowDetails();
