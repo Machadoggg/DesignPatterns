4d78c5b26eb18bb9e0998a6c84d14d2413e42be7
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 09:34:58 -0500

    Add interface builder IHouseBuilder

---
+++ DesignPatterns.CreationalPattern.Builder/IHouseBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.Builder
+{
+    public interface IHouseBuilder
+    {
+        IHouseBuilder BuildWalls();
+        IHouseBuilder BuildDoors();
+        IHouseBuilder BuildWindows();
+        IHouseBuilder BuildRoof();
+        IHouseBuilder BuildGarage();
+        IHouseBuilder BuildSwimPool();
+        IHouseBuilder BuildStatues();
+        IHouseBuilder BuildGarden();
+        House GetHouse();
+    }
+}
