030b176252c0204a0957455aa324238ef3b9e90a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 22:17:13 -0500

    Concrete class ModernSofa

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/ModernSofa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public class ModernSofa : ISofa
+    {
+        public void LieOn()
+        {
+            Console.WriteLine("Lying on a modern sofa.");
+        }
+    }
+}
