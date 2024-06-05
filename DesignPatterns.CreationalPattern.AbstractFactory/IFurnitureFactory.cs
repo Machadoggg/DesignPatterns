2df13b3905c3b1aa4687762df355afbb75c5866b
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-04 22:43:21 -0500

    Add interface for factories (Abstract factory)

---
+++ DesignPatterns.CreationalPattern.AbstractFactory/IFurnitureFactory.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns.CreationalPattern.AbstractFactory.ProductsInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPattern.AbstractFactory
+{
+    public interface IFurnitureFactory
+    {
+        IChair CreateChair();
+
+        ISofa CreateSofa();
+
+        ICoffeTable CreateCoffeTable();
+
+    }
+}
