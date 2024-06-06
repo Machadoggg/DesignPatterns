30b0d1d71b4aba562d9c20f3b698f346dfd29f39
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-06 10:39:38 -0500

    XmlConverter class with ConvertToXml method, old system

---
+++ DesignPatterns.StructuralsPattern.Adapter/XmlConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.StructuralPattern.Adapter
+{
+    public class XmlConverter
+    {
+        public string ConvertToXml(string data)
+        {
+            // Conversion JSON to XML
+            return $"<data>{data}</data>";
+        }
+    }
+}
