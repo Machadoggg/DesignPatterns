00f865cb37a32508274ea4cef59105b147b28da5
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-05 19:05:51 -0500

    Advanced User Form class

---
+++ DesignPatterns.CreationalPattern.Prototype/AdvancedUserForm.cs
@@ -0,0 +1,38 @@
+﻿namespace DesignPatterns.CreationalPattern.Prototype
+{
+    public class AdvancedUserForm : IUserForm
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        public AdvancedUserForm()
+        {
+
+        }
+
+        public AdvancedUserForm(AdvancedUserForm source)
+        {
+            if (source != null)
+            {
+                UserName = source.UserName;
+                Email = source.Email;
+                Address = source.Address;
+                PhoneNumber = source.PhoneNumber;
+            }
+        }
+
+
+        public object Clone()
+        {
+            return new AdvancedUserForm(this);
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Advanced User Form: Username: {UserName}, Email: {Email}, Address: {Address}, Phone: {PhoneNumber}");
+        }
+    }
+}
