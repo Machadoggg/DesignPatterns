430d4133e4db9185a2263f80843913eaa879b9b8
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-06 10:51:54 -0500

    Implement adapter XmlToJsonAdapter

---
+++ DesignPatterns.StructuralsPattern.Adapter/XmlToJsonAdapter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+
+namespace DesignPatterns.StructuralPattern.Adapter
+{
+    public class XmlToJsonAdapter : IJsonConverter
+    {
+        private readonly XmlConverter _xmlConverter;
+
+        public XmlToJsonAdapter()
+        {
+            _xmlConverter = new XmlConverter();
+        }
+
+
+        public string ConvertToJson(string xml)
+        {
+            // first convert XML to JSON using old system
+            string xmlData = _xmlConverter.ConvertToXml(xml);
+
+            // Convert XML to JSON
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(xmlData);
+            string json = JsonConvert.SerializeXmlNode(document);
+            return json;
+        }
+    }
+}
