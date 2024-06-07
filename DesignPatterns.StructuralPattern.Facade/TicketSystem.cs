d06d2b2e3c172c6f0ff7fe15285d81fedf16b1f3
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-07 07:28:35 -0500

    Subsystem tickets sale

---
+++ DesignPatterns.StructuralPattern.Facade/TicketSystem.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.StructuralPattern.Facade
+{
+    public class TicketSystem
+    {
+        public void SellTicket(string movie, int seats)
+        {
+            Console.WriteLine($"Selling {seats} movie tickets: {movie}");
+        }
+    }
+}
