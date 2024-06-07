a21f28c83ce76eb289725c86f4d992969dc7786e
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-07 08:00:10 -0500

    Facade class

---
+++ DesignPatterns.StructuralPattern.Facade/CinemaFacade.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.StructuralPattern.Facade
+{
+    public class CinemaFacade
+    {
+        private TicketSystem _ticketSystem;
+        private MovieSystem _movieSystem;
+        private SnackSystem _snackSystem;
+
+
+        public CinemaFacade()
+        {
+            _ticketSystem = new TicketSystem();
+            _movieSystem = new MovieSystem();
+            _snackSystem = new SnackSystem();
+        }
+
+        public void WatchMovie(string movie, int seats, string snack)
+        {
+            _ticketSystem.SellTicket(movie, seats);
+            _snackSystem.OrderSnack(snack);
+            _movieSystem.PlayMovie(movie);
+            Console.WriteLine($"Enjoy the movie: {movie} with your snack: {snack}");
+        }
+
+
+    }
+}
