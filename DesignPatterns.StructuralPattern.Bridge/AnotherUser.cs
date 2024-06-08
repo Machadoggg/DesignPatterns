f755e4e40508d3d365229e941606930d713e67f4
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 08:00:57 -0500

    Another refined abstraction class AnotherUser

---
+++ DesignPatterns.StructuralPattern.Bridge/AnotherUser.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatterns.StructuralPattern.Bridge
+{
+    public class AnotherUser : User
+    {
+        private string username;
+
+        public AnotherUser(string username, IAuthentication authentication) : base(authentication)
+        {
+            this.username = username;
+        }
+
+        public override void Login(string credential)
+        {
+            if (_authentication.Authenticate(username, credential))
+            {
+                Console.WriteLine($"AnotherUser user {username} logged in successfully.");
+            }
+        }
+    }
+}
