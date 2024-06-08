f7c8f2849d91aa30135531b181e334c56821df69
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 08:06:24 -0500

    Add another user in main

--- DesignPatterns.StructuralPattern.Bridge/Program.cs
+++ DesignPatterns.StructuralPattern.Bridge/Program.cs
@@ -10,9 +10,15 @@ User adminWithToken = new AdminUser("admin2", tokenAuth);
 User userWithPassword = new StandardUser("user1", passwordAuth);
 User userWithToken = new StandardUser("user2", tokenAuth);
 
+User anotherUserWithPassword = new AnotherUser("anotherUser1", passwordAuth);
+User anotherUserWithToken = new AnotherUser("anotherUser2", tokenAuth);
+
 
 adminWithPasswod.Login("adminPassword");
 adminWithToken.Login("adminToken");
 
 userWithPassword.Login("userPassword");
 userWithToken.Login("userToken");
+
+anotherUserWithPassword.Login("anotherUserPassword");
+anotherUserWithToken.Login("anotherUserToken");
