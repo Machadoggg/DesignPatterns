daa5f0bc310566c09f76a89016717a6be32bfd5e
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 07:42:30 -0500

    Refined Abstraction class StandardUser

---
+++ DesignPatterns.StructuralPattern.Bridge/StandardUser.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatterns.StructuralPattern.Bridge
+{
+    public class StandardUser : User
+    {
+        private string username;
+
+        public StandardUser(string username, IAuthentication authentication) : base(authentication) 
+        {
+            this.username = username;
+        }
+
+        public override void Login(string credential)
+        {
+            if (_authentication.Authenticate(username, credential)) 
+            {
+                Console.WriteLine($"Standard user {username} logged in successfully.");
+            }
+        }
+    }
+}
