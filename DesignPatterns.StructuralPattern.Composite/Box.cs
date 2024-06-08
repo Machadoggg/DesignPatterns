dd051219c90ca87f318f69cb093d09a4362aadc5
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 18:32:10 -0500

    Compound class Box with add methods

---
+++ DesignPatterns.StructuralPattern.Composite/Box.cs
@@ -0,0 +1,35 @@
+﻿namespace DesignPatterns.StructuralPattern.Composite
+{
+    public class Box : Component
+    {
+        private List<Component> _components = new List<Component>();
+
+        public Box(string name) : base(name) 
+        {
+            
+        }
+
+
+
+        public void Add(Component component)
+        {
+            _components.Add(component);
+        }
+
+        public void Remove(Component component)
+        {
+            _components.Remove(component);
+        }
+
+
+        public override void Show(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + Name);
+
+            foreach (Component component in _components) 
+            {
+                component.Show(depth + 2);
+            }
+        }
+    }
+}
