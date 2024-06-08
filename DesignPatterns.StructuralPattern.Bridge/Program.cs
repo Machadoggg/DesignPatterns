dcc0187b85e2697cc3894d5caa78eed7f13d0015
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 07:55:00 -0500

    Using pattern in main (client side)

--- DesignPatterns.StructuralPattern.Bridge/Program.cs
+++ DesignPatterns.StructuralPattern.Bridge/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatterns.StructuralPattern.Bridge;
+
+IAuthentication passwordAuth = new PasswordAuthentication();
+IAuthentication tokenAuth = new TokenAuthentication();
+
+
+User adminWithPasswod = new AdminUser("admin1", passwordAuth);
+User adminWithToken = new AdminUser("admin2", tokenAuth);
+
+User userWithPassword = new StandardUser("user1", passwordAuth);
+User userWithToken = new StandardUser("user2", tokenAuth);
+
+
+adminWithPasswod.Login("adminPassword");
+adminWithToken.Login("adminToken");
+
+userWithPassword.Login("userPassword");
+userWithToken.Login("userToken");
