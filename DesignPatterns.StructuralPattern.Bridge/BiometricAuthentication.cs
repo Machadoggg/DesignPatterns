4070b2b147c75ba2b1ab67378f7177cb12e6da5d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 08:20:49 -0500

    Concrete Implementor class BiometricAuthentication

---
+++ DesignPatterns.StructuralPattern.Bridge/BiometricAuthentication.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.StructuralPattern.Bridge
+{
+    public class BiometricAuthentication : IAuthentication
+    {
+        public bool Authenticate(string username, string credential)
+        {
+            Console.WriteLine($"Authentication {username} with biometric system");
+            return true;
+        }
+    }
+}
