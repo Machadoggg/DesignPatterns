8cc8572d7d633bb28483e12b2ce7cadec93346d4
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 07:26:08 -0500

    Abstraction class User

---
+++ DesignPatterns.StructuralPattern.Bridge/User.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.StructuralPattern.Bridge
+{
+    public abstract class User
+    {
+        protected IAuthentication _authentication;
+
+
+        protected User(IAuthentication authentication)
+        {
+            _authentication = authentication;
+        }
+
+        public abstract void Login(string credential);
+    }
+}
