08c6b7fd4f2a99276a249146c37c138b6789c1c8
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 07:36:37 -0500

    Refined Abstraction class AdminUser

---
+++ DesignPatterns.StructuralPattern.Bridge/AdminUser.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatterns.StructuralPattern.Bridge
+{
+    public class AdminUser : User
+    {
+        private string username;
+
+        public AdminUser(string username, IAuthentication authentication) : base(authentication)
+        {
+            this.username = username;
+        }
+
+        public override void Login(string credential)
+        {
+            if (_authentication.Authenticate(username, credential)) 
+            {
+                Console.WriteLine($"Admin user {username} logged in successfully.");
+            }
+        }
+    }
+}
