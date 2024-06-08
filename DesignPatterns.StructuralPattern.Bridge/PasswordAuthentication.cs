251896088a3dd90bb99982ff82bb18519fe4bc2d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 07:19:37 -0500

    Concrete Implementor class PasswordAuthentication

---
+++ DesignPatterns.StructuralPattern.Bridge/PasswordAuthentication.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.StructuralPattern.Bridge
+{
+    public class PasswordAuthentication : IAuthentication
+    {
+        public bool Authenticate(string username, string credential)
+        {
+            Console.WriteLine($"Authenticating {username} with password");
+            return true;
+        }
+    }
+}
