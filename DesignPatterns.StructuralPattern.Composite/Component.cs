8f71a6e30dfbdc042fe31ca1326cb6f85fc6f461
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 18:09:20 -0500

    Abstract class Component

---
+++ DesignPatterns.StructuralPattern.Composite/Component.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.StructuralPattern.Composite
+{
+    public abstract class Component
+    {
+        public string Name { get; set; }
+
+
+        protected Component(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Mostrar(int depth);
+
+    }
+}
