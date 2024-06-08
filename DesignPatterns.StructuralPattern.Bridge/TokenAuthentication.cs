366bf6a16df613f039285c38cdbd1bed64ba2189
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-08 07:22:39 -0500

    Concrete Implementor class TokenAuthentication

---
+++ DesignPatterns.StructuralPattern.Bridge/TokenAuthentication.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.StructuralPattern.Bridge
+{
+    public class TokenAuthentication : IAuthentication
+    {
+        public bool Authenticate(string username, string credential)
+        {
+            Console.WriteLine($"Authentication {username} with token");
+            return true;
+        }
+    }
+}
