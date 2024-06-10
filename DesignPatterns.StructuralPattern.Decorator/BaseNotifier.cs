c0f9bd93a37d9b800c4c3e6ed0672af2e720f727
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 08:34:55 -0500

    Concret class BaseNotifier with Send method implemented

---
+++ DesignPatterns.StructuralPattern.Decorator/BaseNotifier.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.StructuralPattern.Decorator
+{
+    public class BaseNotifier : INotifier
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"Sending basic notification: {message}");
+        }
+    }
+}
