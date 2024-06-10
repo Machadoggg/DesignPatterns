bde997961a38304145c1d1170de78a487d6a4544
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 08:44:34 -0500

    Concret decorator class for SMS

---
+++ DesignPatterns.StructuralPattern.Decorator/SMSNotifier.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.StructuralPattern.Decorator
+{
+    public class SMSNotifier : NotifierDecorator
+    {
+        public SMSNotifier(INotifier notifier) : base(notifier)
+        {
+        }
+
+
+        public override void Send(string message)
+        {
+            base.Send(message);
+            Console.WriteLine($"Sending SMS notification: {message}");
+        }
+    }
+}
