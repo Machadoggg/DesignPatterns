65e2bd214231bd9d89a5b7f9108eeb8e45e9a39c
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 08:53:27 -0500

    Concrete decorator class SLACK

---
+++ DesignPatterns.StructuralPattern.Decorator/SlackNotifier.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.StructuralPattern.Decorator
+{
+    public class SlackNotifier : NotifierDecorator
+    {
+        public SlackNotifier(INotifier notifier) : base(notifier)
+        {
+        }
+
+
+        public override void Send(string message)
+        {
+            base.Send(message);
+            Console.WriteLine($"Sending SLACK notification: {message}");
+        }
+    }
+}
