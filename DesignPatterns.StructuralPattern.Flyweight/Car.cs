56a25792e4f3b82813e31eca5a962ccd5c0283f6
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 11:05:02 -0500

    class that uses Flyweight (Owner, Number)

---
+++ DesignPatterns.StructuralPattern.Flyweight/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.StructuralPattern.Flyweight
+{
+    public class Car
+    {
+        public string Owner { get; }
+        public string Number { get; }
+
+        private CarFlyweight flyweight;
+
+        public Car(string owner, string number, CarFlyweight flyweight)
+        {
+            Owner = owner;
+            Number = number;
+            this.flyweight = flyweight;
+        }
+
+        public void Display()
+        {
+            flyweight.Display(Owner, Number);
+        }
+    }
+}
