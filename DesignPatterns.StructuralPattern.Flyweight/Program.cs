8aec9fc04e8f8263d04d37f16d7cae2fdb3a70eb
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 15:11:54 -0500

    Modified rogram to use the factory and show state

--- DesignPatterns.StructuralPattern.Flyweight/Program.cs
+++ DesignPatterns.StructuralPattern.Flyweight/Program.cs
@@ -1,14 +1,24 @@
 ﻿using DesignPatterns.StructuralPattern.Flyweight;
 
+
 CarFlyweightFactory factory = new CarFlyweightFactory();
 
-CarFlyweight bmwRed = factory.GetFlyweight("BMW", "X5", "Red");
-CarFlyweight audiBlue = factory.GetFlyweight("Audi", "A4", "Blue");
+factory.GetFlyweight("Chevrolet", "Camaro2018", "pink");
+factory.GetFlyweight("Mercedes Benz", "C300", "black");
+factory.GetFlyweight("Mercedes Benz", "C500", "red");
+factory.GetFlyweight("BMW", "M5", "red");
+factory.GetFlyweight("BMW", "X6", "white");
 
-Car car1 = new Car("John Doe", "1234", bmwRed);
-Car car2 = new Car("Jane Smith", "5678", audiBlue);
-Car car3 = new Car("Alice Johnson", "9101", bmwRed); 
+factory.ListFlyweights();
 
+Console.WriteLine("\nClient: Adding a car to database.");
+CarFlyweight flyweight1 = factory.GetFlyweight("BMW", "M5", "red");
+Car car1 = new Car("James Doe", "CL234IR", flyweight1);
 car1.Display();
+
+Console.WriteLine("\nClient: Adding a car to database.");
+CarFlyweight flyweight2 = factory.GetFlyweight("BMW", "X1", "red");
+Car car2 = new Car("James Doe", "CL234IR", flyweight2);
 car2.Display();
-car3.Display();
+
+factory.ListFlyweights();
