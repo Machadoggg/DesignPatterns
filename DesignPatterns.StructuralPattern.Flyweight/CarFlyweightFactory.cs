708e4894e9778b0afce392654a4e406d1585f217
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 15:05:21 -0500

    Modified GetFlyweight method, add ListFlyweights method

--- DesignPatterns.StructuralPattern.Flyweight/CarFlyweightFactory.cs
+++ DesignPatterns.StructuralPattern.Flyweight/CarFlyweightFactory.cs
@@ -2,18 +2,32 @@
 {
     public class CarFlyweightFactory
     {
+
+
         private Dictionary<string, CarFlyweight> flyweights = new Dictionary<string, CarFlyweight>();
 
-
-        public CarFlyweight GetFlyweight(string company, string model, string color) 
+        public CarFlyweight GetFlyweight(string company, string model, string color)
         {
             string key = $"{company}_{model}_{color}";
-
-            if (!flyweights.ContainsKey(key) ) 
+            if (flyweights.ContainsKey(key))
             {
+                Console.WriteLine("FlyweightFactory: Reusing existing flyweight.");
+            }
+            else
+            {
+                Console.WriteLine("FlyweightFactory: Can't find a flyweight, creating new one.");
                 flyweights[key] = new CarFlyweight(company, model, color);
             }
             return flyweights[key];
+        }
+
+        public void ListFlyweights()
+        {
+            Console.WriteLine($"FlyweightFactory: I have {flyweights.Count} flyweights:");
+            foreach (var key in flyweights.Keys)
+            {
+                Console.WriteLine(key);
+            }
         }
     }
 }
