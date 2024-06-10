3c5ed91289e5f17ac66a85ff8e38cc8ce558ad88
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 10:59:26 -0500

    Flyweights factory, creating and storing Flyweight objects.

---
+++ DesignPatterns.StructuralPattern.Flyweight/CarFlyweightFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.StructuralPattern.Flyweight
+{
+    public class CarFlyweightFactory
+    {
+        private Dictionary<string, CarFlyweight> flyweights = new Dictionary<string, CarFlyweight>();
+
+
+        public CarFlyweight GetFlyweight(string company, string model, string color) 
+        {
+            string key = $"{company}_{model}_{color}";
+
+            if (!flyweights.ContainsKey(key) ) 
+            {
+                flyweights[key] = new CarFlyweight(company, model, color);
+            }
+            return flyweights[key];
+        }
+    }
+}
