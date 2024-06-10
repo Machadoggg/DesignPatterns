8fba735fcef2bfebcdf77580d1b6d399c375b33d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 08:38:59 -0500

    Implement base class NotifierDecorator

---
+++ DesignPatterns.StructuralPattern.Decorator/NotifierDecorator.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatterns.StructuralPattern.Decorator
+{
+    public class NotifierDecorator : INotifier
+    {
+        protected INotifier _notifier;
+
+        public NotifierDecorator(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+
+        public virtual void Send(string message)
+        {
+            _notifier.Send(message);
+        }
+    }
+}
