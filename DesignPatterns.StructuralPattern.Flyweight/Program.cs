78e8c002f16b382dbb01e2d201ac3752836d8f6e
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 14:58:00 -0500

    Use Flyweight in main

--- DesignPatterns.StructuralPattern.Flyweight/Program.cs
+++ DesignPatterns.StructuralPattern.Flyweight/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesignPatterns.StructuralPattern.Flyweight;
+
+CarFlyweightFactory factory = new CarFlyweightFactory();
+
+CarFlyweight bmwRed = factory.GetFlyweight("BMW", "X5", "Red");
+CarFlyweight audiBlue = factory.GetFlyweight("Audi", "A4", "Blue");
+
+Car car1 = new Car("John Doe", "1234", bmwRed);
+Car car2 = new Car("Jane Smith", "5678", audiBlue);
+Car car3 = new Car("Alice Johnson", "9101", bmwRed); // Compartido
+
+car1.Display();
+car2.Display();
+car3.Display();
