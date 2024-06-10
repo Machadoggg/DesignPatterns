477a6be7ee8bd8fcf7d27b8fe7b53f16413af218
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-10 10:50:39 -0500

    Flyweight class with Display method

---
+++ DesignPatterns.StructuralPattern.Flyweight/CarFlyweight.cs
@@ -0,0 +1,23 @@
+﻿namespace DesignPatterns.StructuralPattern.Flyweight
+{
+    public class CarFlyweight
+    {
+        public CarFlyweight(string company, string model, string color)
+        {
+            Company = company;
+            Model = model;
+            Color = color;
+        }
+
+        public string Company { get; } = default!;
+        public string Model { get; } = default!;
+        public string Color { get; set; } = default!;
+
+        public void Display(string owner, string number)
+        {
+            Console.WriteLine($"Car {number}: {Company} {Model} in {Color}, owned by {owner}");
+        }
+
+
+    }
+}
