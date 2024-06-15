4d7939f82897a0b8af6478697765f09f806825bf
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-15 07:47:49 -0500

    Implement real class of subject

---
+++ DesignPatterns.StructuralPattern.Proxy/RealUserDatabase.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.StructuralPattern.Proxy
+{
+    public class RealUserDatabase : IUserDatabase
+    {
+        public string GetUserData(int userId)
+        {
+            // Simulation of an expensive database call
+            Console.WriteLine($"Fetching data for user {userId} from the real database.");
+            return $"User data for user {userId}";
+        }
+    }
+}
