ae08d7e52c16393dabc890dae27cf542e4b22bb6
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-15 08:47:48 -0500

    Base class of the handler

---
+++ DesignPatterns.BehaviorPattern.ChainOfResponsibility/User.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.BehaviorPattern.ChainOfResponsibility
+{
+    public abstract class User
+    {
+        protected User _succesor;
+
+
+        public void SetSuccesor(User succesor)
+        { 
+            this._succesor = succesor;
+        }
+
+        public abstract void HandleRequest(SalesRequest request);
+    }
+}
