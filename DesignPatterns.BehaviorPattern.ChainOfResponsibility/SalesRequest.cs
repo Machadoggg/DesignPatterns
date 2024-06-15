dce7d5953f56de09e436b50ae30deff23ca41756
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-15 08:52:41 -0500

    SalesRequest class with 3 propertiies and constructor

---
+++ DesignPatterns.BehaviorPattern.ChainOfResponsibility/SalesRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.BehaviorPattern.ChainOfResponsibility
+{
+    public class SalesRequest
+    {
+        
+        public string RequestType { get; }
+        public double Amount { get; }
+        public string Description { get; }
+
+
+        public SalesRequest(string requestType, double amount, string description)
+        {
+            RequestType = requestType;
+            Amount = amount;
+            Description = description;
+        }
+
+
+
+    }
+}
