c3548a8649d79a708d1e4f003d985e7c8a043605
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-15 09:06:43 -0500

    Create concrete class Director of the  handler

---
+++ DesignPatterns.BehaviorPattern.ChainOfResponsibility/Director.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.BehaviorPattern.ChainOfResponsibility
+{
+    public class Director : User
+    {
+        public override void HandleRequest(SalesRequest request)
+        {
+            if (request.RequestType == "Large Sale")
+            {
+                Console.WriteLine($"{this.GetType().Name} handled request: {request.Description}");
+            }
+            else
+            {
+                Console.WriteLine($"Request: {request.Description} requires further evaluation.");
+            }
+        }
+    }
+}
