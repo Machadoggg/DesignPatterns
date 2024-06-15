744623543948854bc5ee5030a8bef78aec080528
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-15 09:04:43 -0500

    Create concrete class SalesManager of the handler

---
+++ DesignPatterns.BehaviorPattern.ChainOfResponsibility/SalesManager.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.BehaviorPattern.ChainOfResponsibility
+{
+    public class SalesManager : User
+    {
+        public override void HandleRequest(SalesRequest request)
+        {
+            if (request.RequestType == "Medium Sale")
+            {
+                Console.WriteLine($"{this.GetType().Name} handled request: {request.Description}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+}
