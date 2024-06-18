67798935ee46cb78b013193b1174f18b7fd90854
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 23:51:58 -0500

    Concret class IShippingStrategy, implement the strategy interface IShippingStrategy

---
+++ DesignPatterns.BehaviorPattern.Strategy/GroundShippingStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.BehaviorPattern.Strategy
+{
+    public class GroundShippingStrategy : IShippingStrategy
+    {
+        public double CalculateShippingCost(Order order)
+        {
+            return order.weight * 0.5; // cost for weight for ground shipping 
+        }
+    }
+}
