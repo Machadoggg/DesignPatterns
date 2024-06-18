f97a6d984bb557d7c5e2306d5f3d9c1075761563
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 23:59:17 -0500

    Concret class SeaShippingStrategy, implement the strategy interface IShippingStrategy

---
+++ DesignPatterns.BehaviorPattern.Strategy/SeaShippingStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.BehaviorPattern.Strategy
+{
+    public class SeaShippingStrategy : IShippingStrategy
+    {
+        public double CalculateShippingCost(Order order)
+        {
+            return order.Weight * 0.2; // Cost for weight for sea shipping
+        }
+    }
+}
