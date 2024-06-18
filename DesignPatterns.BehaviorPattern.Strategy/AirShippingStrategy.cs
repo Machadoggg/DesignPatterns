f79bb86fe94f79ccfd46890d42ff48e5a7474d9b
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-17 23:56:29 -0500

    Concret class AirShippingStrategy, implement  the strategy interface IShippingStrategy

---
+++ DesignPatterns.BehaviorPattern.Strategy/AirShippingStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.BehaviorPattern.Strategy
+{
+    public class AirShippingStrategy : IShippingStrategy
+    {
+        public double CalculateShippingCost(Order order)
+        {
+            return order.Weight * 1.0; // Cost for weight for air shipping
+        }
+    }
+}
