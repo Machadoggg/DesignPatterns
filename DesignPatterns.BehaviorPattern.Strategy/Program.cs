dbb1d9da694be1d82f7811d9a02d783474678db2
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-18 00:21:02 -0500

    Calculate shipping cost according strategy in main

--- DesignPatterns.BehaviorPattern.Strategy/Program.cs
+++ DesignPatterns.BehaviorPattern.Strategy/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using DesignPatterns.BehaviorPattern.Strategy;
+
+OrderContext order = new OrderContext { Weight = 10 };
+
+
+// Ground shipping
+order.ShippingStrategy = new GroundShippingStrategy();
+Console.WriteLine($"Ground shipping cost: {order.CalculateShippingCost()}");
+
+
+// Air shipping 
+order.ShippingStrategy = new AirShippingStrategy();
+Console.WriteLine($"Air shipping cost: {order.CalculateShippingCost()}");
+
+
+// Sea shipping 
+order.ShippingStrategy = new SeaShippingStrategy();
+Console.WriteLine($"Sea shipping cost: {order.CalculateShippingCost()}");
