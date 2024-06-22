67946fddd257115eeb4c79d493b8004a9eefb25e
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-21 23:53:43 -0500

    Create instances NewsAgency, Subscriber and subscribe observers to subject

--- DesignPatterns.BehaviorPattern.Observer/Program.cs
+++ DesignPatterns.BehaviorPattern.Observer/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// Create agency news
+using DesignPatterns.BehaviorPattern.Observer;
+
+NewsAgency newsAgency = new NewsAgency();
+
+// Create subscriptors
+Subscriber subscriber1 = new Subscriber("Subscriber 1");
+Subscriber subscriber2 = new Subscriber("Subscriber 2");
+
+// Add subscriptors to agency news
+newsAgency.Attach(subscriber1);
+newsAgency.Attach(subscriber2);
+
+// Add news
+newsAgency.AddNews("Breaking News: New Observer Pattern Example Released!");
+
+// Delete a subscriptor
+newsAgency.Detach(subscriber1);
+
+// Add more newss
+newsAgency.AddNews("More News: Observer Pattern Example Updated!");
