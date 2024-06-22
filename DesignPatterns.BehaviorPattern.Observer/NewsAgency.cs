0725cd07350918e3203b7782a9a7a3aa2f325b86
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-21 23:44:39 -0500

    Add concret class NewsAgency

---
+++ DesignPatterns.BehaviorPattern.Observer/NewsAgency.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns.BehaviorPattern.Observer
+{
+    public class NewsAgency : ISubject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+        private string latestNews;
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(latestNews);
+            }
+        }
+
+        public void AddNews(string news)
+        {
+            latestNews = news;
+            Notify();
+        }
+
+    }
+}
